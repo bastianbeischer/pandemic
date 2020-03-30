8d7ea180c7a7f34da2334b9b556bb3ee05f7d12a
Bastian Beischer <beischer@physik.rwth-aachen.de>
2020-03-30 13:52:39 +0200

    Continue to improve pandemic.py.

--- pandemic.py
+++ pandemic.py
@@ -28,37 +28,67 @@ class SimpleCompleter(object):
 
 class PandemicInfections(object):
 
-  def __init__(self, cities):
-    self.cities = cities
-    self.stack = [copy.copy(cities)]
+  def __init__(self, cities_file, state_filename='state.txt'):
+    self.cities = []
+    self.stack = []
     self.cards_drawn = []
-    self.current_pile = self.stack[-1]
+    self.state_filename = state_filename
+    self.setup(cities_file)
+
+  def setup(self, cities_file):
+    self.read_cities(cities_file)
+    self.stack = [copy.copy(self.cities)]
+    # Register the completer function and bind tab
+    options = copy.copy(self.cities)
+    options = sorted(set(options), key=options.index)
+    options.append('READ')
+    options.append('EPIDEMIC')
+    readline.set_completer(SimpleCompleter(options).complete)
+    readline.parse_and_bind('tab: complete')
+
+  def read_cities(self, filename):
+    # Read input file with city names.
+    self.cities = []
+    with open(filename, 'r') as f:
+      for line in f:
+        line = line.strip('\n')
+        if not line or line.startswith('#'):
+          continue
+        if '*' in line:
+          n, city = line.split('*')
+          self.cities += int(n) * [city]
+        else:
+          self.cities += [line]
 
   def draw_card(self, line):
+    # Draw card from the top of the stack and add it to the discard pile.
     self.cards_drawn.append(line)
-    assert(self.current_pile)
-    self.current_pile.remove(line)
-    if not self.current_pile:
-      self.current_pile = self.stack.pop()
+    assert(self.stack[-1])
+    self.stack[-1].remove(line)
+    if not self.stack[-1]:
+      self.stack.pop()
 
   def epidemic(self):
     question = 'Which city was drawn from the bottom in the Epidemic? '
     impossible = 'This is impossible!'
+    # Draw card from front of the stack ("the bottom")
     line = input(question)
     assert(self.stack)
     front_pile = self.stack[0]
-    self.stack.remove(front_pile)
     while not line in front_pile:
       print(impossible)
       line = input(question)
-    front_pile.remove(line)
     self.cards_drawn.append(line)
-    if front_pile:
-      self.stack.insert(0, front_pile)
+    # Remove card from front pile and remove it from the stack if it is now empty.
+    front_pile.remove(line)
+    if not front_pile:
+      del self.stack[0]
+    # Push discard pile on stack and reset it.
     self.stack.append(sorted(self.cards_drawn))
     self.cards_drawn = []
 
   def print_state(self, f=sys.stdout):
+    # Print the draw deck with sections
     i = 0
     print('', file=f)
     print('############################', file=f)
@@ -66,9 +96,11 @@ class PandemicInfections(object):
     for x in self.stack:
       for city in x:
         print(i, city, file=f)
-      print('----------------------------', file=f)
       i += 1
+      if i != len(self.stack):
+        print('----------------------------', file=f)
     print('############################', file=f)
+    # Print the discard pile
     print('', file=f)
     print('############################', file=f)
     print('###       Discard        ###', file=f)
@@ -77,16 +109,17 @@ class PandemicInfections(object):
     print('############################', file=f)
 
   def write_state(self):
-    with open('state.txt', 'w') as f:
+    # Write the current state to disk
+    with open(self.state_filename, 'w') as f:
       self.print_state(f=f)
 
   def read_state(self):
+    # Read the current state from disk
     self.stack = []
     self.cards_drawn = []
-
     prev_level = 0
     temp = []
-    with open('state.txt', 'r') as f:
+    with open(self.state_filename, 'r') as f:
       for line in f:
         line = line.strip('\n')
         if line.startswith('#') or line.startswith('-') or not line:
@@ -107,9 +140,9 @@ class PandemicInfections(object):
         self.stack.append(temp)
 
   def calculate_probability(self, city, N):
+    # Calculate the probability to draw city at least once in N draws.
     toy_stack = copy.deepcopy(self.stack)
     toy_pile = toy_stack.pop()
-
     total_prob = 0.0
     for i in range(N):
       total = len(toy_pile)
@@ -129,66 +162,42 @@ class PandemicInfections(object):
     return total_prob
 
   def print_probabilities(self):
+    # Print probabilities
     print()
     print('%-15s %6d %6d %6d %6d %6d' % ('Name', 1, 2, 3, 4, 5))
-    for x in sorted(set(self.cities)):
+    print('--------------------------------------------------')
+    for x in sorted(set(self.cities), key=self.cities.index):
       line = '%-15s ' % x
       for n in range(1,6):
         p = self.calculate_probability(x, n)
         line += "%5.1f%% " % (100.0 * p)
       print(line)
-    print()
 
-  def input_loop(self):
-    question = 'Please enter the name of the city which was drawn or "Epidemic": '
+  def run(self):
+    # The main input loop
+    question = 'Please enter the name of the city which was drawn or "EPIDEMIC/READ": '
     impossible = 'This is impossible!'
-    line = ''
     while True:
-
       # Get new input
+      print()
       line = input(question)
-      while line != 'Epidemic' and line != 'Read' and not line in self.current_pile:
+      while line != 'EPIDEMIC' and line != 'READ' and not line in self.stack[-1]:
         print(impossible)
         line = input(question)
-
       # Process
-      if line == 'Read':
+      if line == 'READ':
         self.read_state()
-      elif line == 'Epidemic':
+      elif line == 'EPIDEMIC':
         self.epidemic()
       else:
         self.draw_card(line)
-
-      # Print current state
+      # Print current state and probabilities, write state to disk
       self.print_state()
-      self.write_state()
       self.print_probabilities()
-
-cities = []
-
-# Black
-cities += 3*['Kairo']
-cities += 3*['Istanbul']
-cities += 3*['Tripolis']
-
-# Blue
-cities += 3*['London']
-cities += 3*['NewYork']
-cities += 3*['Washington']
-
-# Yellow
-cities += 3*['Jacksonville']
-cities += 3*['SaoPaolo']
-cities += 3*['Lagos']
-
-# Register the completer function and bind tab
-options = copy.copy(cities)
-options.append('Read')
-options.append('Epidemic')
-options = sorted(set(options))
-readline.set_completer(SimpleCompleter(options).complete)
-readline.parse_and_bind('tab: complete')
+      self.write_state()
 
 # Start the input loop
-p = PandemicInfections(cities=cities)
-p.input_loop()
+cities_file = sys.argv[1]
+output_file = sys.argv[2] if len(sys.argv) > 2 else 'state.txt'
+p = PandemicInfections(cities_file=cities_file, state_filename=output_file)
+p.run()
